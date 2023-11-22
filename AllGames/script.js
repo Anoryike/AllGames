51290d0fb57b53609e56dbf25d67d7ef8352da0e
Anoryike <149113497+Anoryike@users.noreply.github.com>
2023-11-22 19:13:02 +0200

    Add files via upload

---
+++ AllGames/script.js
@@ -0,0 +1,11 @@
+function navigateToApp(appName) {
+    if (appName === 'application1') {
+      window.location.href = 'https://anoryike.github.io/MagicBall/magic_ball/index7.html';
+    } else if (appName === 'application2') {
+      window.location.href = 'https://anoryike.github.io/game/Game/index8.html';
+    } else if (appName === 'application3') {
+      window.location.href = 'https://anoryike.github.io/CardGame/CardGame/index9.html';
+    }else if (appName === 'application4') {
+        window.location.href = 'https://anoryike.github.io/One-armedBandit/One-armed_bandit/index10.html';
+    }
+  }
